1ec69c55d8de5ba929e2d6b3932748c1e60cc6f5
Chris Weyl <cweyl@alumni.drew.edu>
2020-01-14 17:39:13 -0600

    Add `NewConfigTemplateFromFile()`

--- helper/configtemplate/config_template.go
+++ helper/configtemplate/config_template.go
@@ -4,33 +4,23 @@
 package configtemplate
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"os/signal"
-	"runtime"
-	"strings"
-
 	"github.com/imdario/mergo"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
-
 	"gitlab.com/gitlab-org/gitlab-runner/common"
 )
 
 type ConfigTemplate struct {
 	*common.Config
 
-	ConfigFile string `long:"config" env:"TEMPLATE_CONFIG_FILE" description:"Path to the configuration template file"`
+	configFile string `long:"config" env:"TEMPLATE_CONFIG_FILE" description:"Path to the configuration template file"`
 }
 
 func (c *ConfigTemplate) Enabled() bool {
-	return c.ConfigFile != ""
+	return c.configFile != ""
 }
 
 func (c *ConfigTemplate) MergeTo(config *common.RunnerConfig) error {
-	err := c.loadConfigTemplate()
+	err := c.LoadConfigTemplate()
 	if err != nil {
 		return errors.Wrap(err, "couldn't load configuration template file")
 	}
@@ -47,10 +37,40 @@ func (c *ConfigTemplate) MergeTo(config *common.RunnerConfig) error {
 	return nil
 }
 
-func (c *ConfigTemplate) loadConfigTemplate() error {
+func NewConfigTemplateFromFile(cfgFile string) (*ConfigTemplate, error) {
+	ct := ConfigTemplate{
+		configFile: cfgFile,
+	}
+	if err := ct.LoadConfigTemplate(); err != nil {
+		return nil, err
+	}
+	return &ct, nil
+}
+
+// func NewConfigTemplateFromString(cfg string) (*ConfigTemplate, error) {
+// 	// https://godoc.org/github.com/hashicorp/terraform-plugin-sdk/helper/pathorcontents
+// 	templateStr := pathorcontents.Read(templateData)
+// 	tmpfile, err := ioutil.TempFile("", "runnercfgtemplate*")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer os.Remove(tmpfile.Name())
+// 	if _, err = tmpfile.WriteContent(templateStr); err != nil {
+// 		return err
+// 	}
+// 	ct := ConfigTemplate{
+// 		configFile: cfgFile,
+// 	}
+// 	if err := ct.LoadConfigTemplate(); err != nil {
+// 		return nil, err
+// 	}
+// 	return ct, nil
+// }
+
+func (c *ConfigTemplate) LoadConfigTemplate() error {
 	config := common.NewConfig()
 
-	err := config.LoadConfig(c.ConfigFile)
+	err := config.LoadConfig(c.configFile)
 	if err != nil {
 		return err
 	}
@@ -58,55 +78,4 @@ func (c *ConfigTemplate) loadConfigTemplate() error {
 	c.Config = config
 
 	return nil
-}
-
-type RegisterCommand struct {
-	context    *cli.Context
-	network    common.Network
-	reader     *bufio.Reader
-	registered bool
-
-	configOptions
-
-	ConfigTemplate ConfigTemplate `namespace:"template"`
-
-	TagList           string `long:"tag-list" env:"RUNNER_TAG_LIST" description:"Tag list"`
-	NonInteractive    bool   `short:"n" long:"non-interactive" env:"REGISTER_NON_INTERACTIVE" description:"Run registration unattended"`
-	LeaveRunner       bool   `long:"leave-runner" env:"REGISTER_LEAVE_RUNNER" description:"Don't remove runner if registration fails"`
-	RegistrationToken string `short:"r" long:"registration-token" env:"REGISTRATION_TOKEN" description:"Runner's registration token"`
-	RunUntagged       bool   `long:"run-untagged" env:"REGISTER_RUN_UNTAGGED" description:"Register to run untagged builds; defaults to 'true' when 'tag-list' is empty"`
-	Locked            bool   `long:"locked" env:"REGISTER_LOCKED" description:"Lock Runner for current project, defaults to 'true'"`
-	AccessLevel       string `long:"access-level" env:"REGISTER_ACCESS_LEVEL" description:"Set access_level of the runner to not_protected or ref_protected; defaults to not_protected"`
-	MaximumTimeout    int    `long:"maximum-timeout" env:"REGISTER_MAXIMUM_TIMEOUT" description:"What is the maximum timeout (in seconds) that will be set for job when using this Runner"`
-	Paused            bool   `long:"paused" env:"REGISTER_PAUSED" description:"Set Runner to be paused, defaults to 'false'"`
-
-	common.RunnerConfig
-}
-
-type AccessLevel string
-
-const (
-	NotProtected AccessLevel = "not_protected"
-	RefProtected AccessLevel = "ref_protected"
-)
-
-const (
-	defaultDockerWindowCacheDir = "c:\\cache"
-)
-
-// func (s *RegisterCommand) addRunner(runner *common.RunnerConfig) {
-// 	s.config.Runners = append(s.config.Runners, runner)
-// }
-
-func (s *RegisterCommand) mergeTemplate() {
-	if !s.ConfigTemplate.Enabled() {
-		return
-	}
-
-	logrus.Infof("Merging configuration from template file %q", s.ConfigTemplate.ConfigFile)
-
-	err := s.ConfigTemplate.MergeTo(&s.RunnerConfig)
-	if err != nil {
-		logrus.WithError(err).Fatal("Could not handle configuration merging from template file")
-	}
 }
