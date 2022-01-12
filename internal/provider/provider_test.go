d7cb7ff89912442762dbc13b05be6767c12e43ca
Chris Weyl <cweyl@alumni.drew.edu>
2022-01-12 12:07:38 -0600

    "fix" our provider tests
    ...for a very loose value of "fix" that roughly relates to "complies
    and runs without exploding"

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -10,30 +10,28 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-var testAccProviders map[string]terraform.ResourceProvider
-var testAccProvider *schema.Provider
-
-func init() {
-	testAccProvider = Provider()
-	testAccProviders = map[string]terraform.ResourceProvider{
-		"gitlabci": testAccProvider,
-	}
+// providerFactories are used to instantiate a provider during acceptance
+// testing.  The factory function will be invoked for every Terraform CLI
+// command executed to create a provider server to which the CLI can reattach.
+var providerFactories = map[string]func() (*schema.Provider, error){
+	"gitlabci": func() (*schema.Provider, error) {
+		return New("dev")(), nil
+	},
 }
 
 func TestProvider(t *testing.T) {
-	if err := Provider().InternalValidate(); err != nil {
+	if err := New("dev")().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
 
-func TestProvider_impl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
-}
-
 func testAccPreCheck(t *testing.T) {
+	// You can add code here to run prior to any test case execution, for
+	// example assertions about the appropriate environment variables being
+	// set are common to see in a pre-check function.
+	//
 	// if v := os.Getenv("GITLAB_TOKEN"); v == "" {
 	// 	t.Fatal("GITLAB_TOKEN must be set for acceptance tests")
 	// }
