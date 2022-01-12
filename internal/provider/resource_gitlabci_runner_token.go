4aa703757d01a902a4bc9e5d9fd342a99debfdcb
Chris Weyl <cweyl@alumni.drew.edu>
2022-01-12 11:09:24 -0600

    Add a resource Description

--- internal/provider/resource_gitlabci_runner_token.go
+++ internal/provider/resource_gitlabci_runner_token.go
@@ -14,11 +14,26 @@ import (
 	"github.com/parnurzeal/gorequest"
 )
 
+const resourceGitlabRunnerDescription = "The `gitlabci_runner` resource " +
+	`allows the trivial creation of a runner
+token using a runner registration token, without requiring authentication to
+the GitLab instance itself.  It does this by using the [runner registration
+token](https://docs.gitlab.com/runner/register/) of the
+project/group/instance, rather than the authentication credentials of any
+specific user.
+
+Note that, once created, the registration is immutable: any changes will
+result in the resource being destroyed and recreated.
+
+See the [Registering a runner](https://registry.terraform.io/providers/rsrchboy/gitlabci/latest/docs/guides/registering-a-runner)
+guide for more information.`
+
 func resourceGitlabRunner() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceGitlabRunnerCreate,
-		Read:   resourceGitlabRunnerRead,
-		Delete: resourceGitlabRunnerDelete,
+		Create:      resourceGitlabRunnerCreate,
+		Read:        resourceGitlabRunnerRead,
+		Delete:      resourceGitlabRunnerDelete,
+		Description: resourceGitlabRunnerDescription,
 
 		Schema: map[string]*schema.Schema{
 			"registration_token": {
