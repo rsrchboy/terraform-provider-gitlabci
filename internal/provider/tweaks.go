dd04bfd31bab653a370d1568a67f652c806e9027
Chris Weyl <cweyl@alumni.drew.edu>
2022-01-23 23:05:24 -0600

    More tweaks!
    * flag more sensitive attributes as sensitive
    * some minimal, trivial validations

--- internal/provider/tweaks.go
+++ internal/provider/tweaks.go
@@ -1,6 +1,8 @@
 package provider
 
 import (
+	"regexp"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
@@ -9,19 +11,12 @@ import (
 
 func init() {
 
-	// validation, etc
-	configDataSourceRawSchema["concurrent"].ValidateFunc = validation.IntAtLeast(1)
-	configDataSourceRawSchema["check_interval"].ValidateFunc = validation.IntAtLeast(0)
-	configDataSourceRawSchema["log_level"].ValidateFunc =
-		validation.StringInSlice([]string{"panic", "fatal", "error", "warning", "info", "debug"}, false)
-	configDataSourceRawSchema["log_format"].ValidateFunc =
-		validation.StringInSlice([]string{"runner", "text", "json"}, false)
-
 	// computed attributes
 	configDataSourceRawSchema["config"] = &schema.Schema{
 		Type:        schema.TypeString,
 		Computed:    true,
-		Description: "The computed runner configuration (toml)",
+		Sensitive:   true,
+		Description: "The computed runner configuration (toml).  This attribute is marked sensitive as it may include authentication tokens, cache bucket keys, etc.",
 	}
 	configDataSourceRawSchema["id"] = &schema.Schema{
 		Type:        schema.TypeString,
@@ -33,11 +28,65 @@ func init() {
 	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).Schema["config_template"] = &schema.Schema{
 		Type:        schema.TypeString,
 		Optional:    true,
-		Description: "Configuration template (toml)",
+		Sensitive:   true,
+		Description: "Configuration template (toml).  If included, this toml will be used as the base of this runner's configuration.  This attribute is marked sensitive as it may include authentication tokens, cache bucket keys, etc.",
 	}
 
 	// FIXME make sure this flows out into our generated toml output iff set
 	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
-		Schema["runner_credentials"].Elem.(*schema.Resource).
 		Schema["token"].Sensitive = true
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["cache"].Elem.(*schema.Resource).
+		Schema["s3"].Elem.(*schema.Resource).
+		Schema["secret_key"].Sensitive = true
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["cache"].Elem.(*schema.Resource).
+		Schema["gcs"].Elem.(*schema.Resource).
+		Schema["private_key"].Sensitive = true
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["cache"].Elem.(*schema.Resource).
+		Schema["azure"].Elem.(*schema.Resource).
+		Schema["account_key"].Sensitive = true
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["kubernetes"].Elem.(*schema.Resource).
+		Schema["bearer_token"].Sensitive = true
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["ssh"].Elem.(*schema.Resource).
+		Schema["password"].Sensitive = true
+
+	// validations
+	configDataSourceRawSchema["concurrent"].ValidateFunc = validation.IntAtLeast(1)
+	configDataSourceRawSchema["check_interval"].ValidateFunc = validation.IntAtLeast(0)
+	configDataSourceRawSchema["log_level"].ValidateFunc =
+		validation.StringInSlice([]string{"panic", "fatal", "error", "warning", "info", "debug"}, false)
+	configDataSourceRawSchema["log_format"].ValidateFunc =
+		validation.StringInSlice([]string{"runner", "text", "json"}, false)
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["url"].ValidateFunc = validation.IsURLWithHTTPorHTTPS
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["clone_url"].ValidateFunc = validation.IsURLWithHTTPorHTTPS
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["executor"].ValidateFunc =
+		validation.StringInSlice([]string{
+			"custom",
+			"docker",
+			"docker+machine",
+			"docker-ssh",
+			"docker-ssh+machine",
+			"docker-windows",
+			"kubernetes",
+			"parallels",
+			"shell",
+			"ssh",
+			"virtualbox",
+		}, false)
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["shell"].ValidateFunc =
+		validation.StringInSlice([]string{"bash", "sh", "powershell", "pwsh"}, false)
+	configDataSourceRawSchema["runners"].Elem.(*schema.Resource).
+		Schema["machine"].Elem.(*schema.Resource).
+		Schema["machine_name"].ValidateFunc =
+		validation.StringMatch(regexp.MustCompile(`.*%s.*`), "string must include %s")
 }
+
+// vim : set nowrap :
